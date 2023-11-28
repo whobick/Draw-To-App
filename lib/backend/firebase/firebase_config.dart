4508a3bbc072347c18e128e200de9e5d0dabbecd
Will Hobick <50713619+whobick@users.noreply.github.com>
2023-11-28 08:59:19 -0500

    Update firebase_config.dart

--- lib/backend/firebase/firebase_config.dart
+++ lib/backend/firebase/firebase_config.dart
@@ -5,12 +5,12 @@ Future initFirebase() async {
   if (kIsWeb) {
     await Firebase.initializeApp(
         options: const FirebaseOptions(
-            apiKey: "AIzaSyAEJRH7ZdaCFrRBvImQ-_D5nTHB8K55LOs",
-            authDomain: "draw-to-app-9zzzv5.firebaseapp.com",
-            projectId: "draw-to-app-9zzzv5",
-            storageBucket: "draw-to-app-9zzzv5.appspot.com",
-            messagingSenderId: "323839435166",
-            appId: "1:323839435166:web:7a03f56aaa75350ee1768f"));
+            apiKey: "[FILL_IN]",
+            authDomain: "[FILL_IN]",
+            projectId: "[FILL_IN]",
+            storageBucket: "[FILL_IN]",
+            messagingSenderId: "[FILL_IN]",
+            appId: "[FILL_IN]"));
   } else {
     await Firebase.initializeApp();
   }
